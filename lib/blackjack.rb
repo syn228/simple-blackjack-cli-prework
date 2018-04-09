dd2063b3781ec87048ff9cb3cb52dd42d7b5457a
Aaron Na <syn228@nyu.edu>
2018-04-09 22:50:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,7 +50,7 @@ end
 #####################################################
 
 def runner
-  welcome
+  puts welcome
   card_total = initial_round
   until (card_total > 21)
   card_total = hit?(card_total)
