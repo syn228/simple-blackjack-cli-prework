8f3a04fc81bc78b7a74fe1d1d06b8fbbb637419e
Aaron Na <syn228@nyu.edu>
2018-04-09 22:56:13 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,7 +50,7 @@ end
 #####################################################
 
 def runner
-  puts "Welcome to the Blackjack Table"
+  welcome
   card_total = initial_round
   until (card_total > 21)
   card_total = hit?(card_total)
