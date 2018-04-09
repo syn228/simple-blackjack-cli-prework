a91283ed660a590f4a52ba1eddcd05b25c550de8
Aaron Na <syn228@nyu.edu>
2018-04-09 23:04:09 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,7 +26,7 @@ def initial_round
     card_one = deal_card
     card_two = deal_card
     card_total = card_one + card_two
-    display_card_total(card_total)
+    return display_card_total(card_total)
   
 
 end
