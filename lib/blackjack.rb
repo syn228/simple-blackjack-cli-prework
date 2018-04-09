6e2256d71e2825f7d4cbaa3fe95bbaebb0786cfd
Aaron Na <syn228@nyu.edu>
2018-04-09 22:53:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,7 +49,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
+def runner(card_total)
   welcome
   initial_round
   until (card_total > 21)
